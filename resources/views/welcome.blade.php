021415e613fbc0d47afdd0d40dcd37fb6fa2485b
arielrecto04 <ariel.recto@innovatotec.com>
2024-12-06 11:01:25 +0800

    add post in homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,15 @@
 <x-landing>
-    <h1 class="ml1" x-data="textAnimation">
-        <span class="text-wrapper eagle-lake-regular">
-            <span class="line line1 border-red-500 hidden md:flex"></span>
-            <span class="letters text-red-500 text-2xl md:text-4xl lg:text-5xl">Merry Christmas!</span>
-            <span class="line line2 hidden md:flex"></span>
-        </span>
-    </h1>
+    <div class="flex flex-col items-center gap-5">
+        <h1 class="ml1" x-data="textAnimation">
+            <span class="text-wrapper eagle-lake-regular">
+                <span class="line line1 border-red-500 hidden md:flex"></span>
+                <span class="letters text-red-500 text-2xl md:text-4xl lg:text-6xl">Merry Christmas!</span>
+                <span class="line line2 hidden md:flex"></span>
+            </span>
+        </h1>
+
+        <img src="{{asset('images/poster.png')}}" alt="" class="w-5/6 lg:w-1/2 aspect-auto rounded-lg shadow-xl
+         hover:scale-105 hover:shadow-red-500 duration-700 z-20">
+    </div>
+    
 </x-landing>
