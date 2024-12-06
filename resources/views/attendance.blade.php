d911d4c140e331cf830a51de518c9a78abad88c4
arielrecto04 <ariel.recto@innovatotec.com>
2024-12-06 09:59:23 +0800

    fix attendance card

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -1,11 +1,10 @@
 <x-landing>
     <form action="{{ route('attendance.store') }}" method="POST"
-        class="flex flex-col gap-2 bg-white shadow-lg p-5 rounded-lg w-full md:w-1/2 lg:w-2/5 z-20"
-        x-data="timeIn">
+        class="flex flex-col gap-10 bg-white shadow-lg rounded-lg w-full md:w-1/2 lg:w-2/5 z-20" x-data="timeIn">
 
         @csrf
 
-        <h1 class="text-4xl text-center py-4 font-bold text-red-500 tracking-wider uppercase bg-gray-50 border-y border-red-500">
+        <h1 class="text-4xl text-center py-4 font-bold text-white tracking-wider uppercase bg-red-500 rounded-t-lg">
             Attendance
         </h1>
 
@@ -46,65 +45,71 @@
                 </div>
 
             </div>
-            <div class="relative group" x-show="!selectedEmployee">
-                <button @click="toggleDropdown" type="button"
-                    class="inline-flex justify-center w-full
-                     px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-red-500 
-                     rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
+
+            <div class="grid grid-cols-2 grid-flow-row gap-2 w-full p-5">
+                <div class="relative group" x-show="!selectedEmployee">
+                    <button @click="toggleDropdown" type="button"
+                        class="inline-flex justify-center w-full
+                         px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-red-500 
+                         rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2
+                          focus:ring-offset-gray-100 focus:ring-blue-500">
 
 
-                    <span class="mr-2 text-red-500 text-2xl" x-show="!isOpen">Open Dropdown</span>
-                    <span class="mr-2 text-red-500 text-2xl" x-show="isOpen">Close Dropdown</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20"
-                        fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd"
-                            d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </button>
-                <div x-show="isOpen" x-init=" laodEmployees({{ json_encode($employees) }})"
-                    class="absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5
-                    w-full p-1 space-y-1 overflow-y-auto max-h-44">
-                    <!-- Search input -->
-                    <input id="search-input"
-                        class="block w-full sticky top-0 px-4 py-2 text-gray-800 border rounded-md  border-gray-300 focus:outline-none"
-                        type="text" x-model.debounce="search" placeholder="Search..." autocomplete="off">
-                    <!-- Dropdown content goes here -->
-                    <template x-if="!search">
-                        <template x-for="employee in employees">
-                            <button type="button" @click="pickEmployee(employee)"
-                                class="block px-4 py-2 text-gray-700 hover:bg-gray-100 
-                                 w-full active:bg-blue-100 cursor-pointer rounded-md">
-                                <span x-text="employee.name"></span>
-                            </button>
+                        <span class="mr-2 text-red-500 text-xl" x-show="!isOpen">Open Dropdown</span>
+                        <span class="mr-2 text-red-500 text-xl" x-show="isOpen">Close Dropdown</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20"
+                            fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd"
+                                d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                    <div x-show="isOpen" x-init=" laodEmployees({{ json_encode($employees) }})"
+                        class="absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5
+                        w-full p-1 space-y-1 overflow-y-auto max-h-44">
+                        <!-- Search input -->
+                        <input id="search-input"
+                            class="block w-full sticky top-0 px-4 py-2 text-gray-800 border rounded-md  border-gray-300 focus:outline-none"
+                            type="text" x-model.debounce="search" placeholder="Search..." autocomplete="off">
+                        <!-- Dropdown content goes here -->
+                        <template x-if="!search">
+                            <template x-for="employee in employees">
+                                <button type="button" @click="pickEmployee(employee)"
+                                    class="block px-4 py-2 text-gray-700 hover:bg-gray-100 
+                                     w-full active:bg-blue-100 cursor-pointer rounded-md">
+                                    <span x-text="employee.name"></span>
+                                </button>
+                            </template>
                         </template>
-                    </template>
-                    <template x-if="search">
-                        <template x-for="employee in results">
-                            <button type="button" @click="pickEmployee(employee)"
-                                class="block px-4 py-2
-                                w-full text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md">
-                                <span x-text="employee.name"></span>
-                            </button>
+                        <template x-if="search">
+                            <template x-for="employee in results">
+                                <button type="button" @click="pickEmployee(employee)"
+                                    class="block px-4 py-2
+                                    w-full text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md">
+                                    <span x-text="employee.name"></span>
+                                </button>
+                            </template>
                         </template>
-                    </template>
 
 
+                    </div>
                 </div>
+                <template x-if="selectedEmployee?.attendance == null">
+                    <button class="btn btn-error text-white text-lg text-xl">Submit</button>
+                </template>
+
+                <template x-if="selectedEmployee?.attendance != null">
+                    <button class="btn btn-error text-xl" disabled>Submit</button>
+                </template>
             </div>
+
         </label>
         {{-- <div class="flex flex-col gap-2">
             <label for="" class="text-gray-400">Full Name</label>
             <input type="text" class="input border-red-500" disabled placeholder="ex: Ariel Recto">
         </div> --}}
 
-        <template x-if="selectedEmployee?.attendance == null">
-            <button class="btn btn-error text-white text-lg">Submit</button>
-        </template>
 
-        <template x-if="selectedEmployee?.attendance != null">
-            <button class="btn btn-error" disabled>Submit</button>
-        </template>
 
 
     </form>
