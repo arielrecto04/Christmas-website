a56ad03b10facaa02409daf588281db8a956902f
Cesar Pinero <cpinero522003@gmail.com>
2025-11-10 13:59:22 +0800

    fixed: wrong id hahaha

--- database/migrations/2025_11_10_031136_create_candidate_votes_table.php
+++ database/migrations/2025_11_10_031136_create_candidate_votes_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\CandidateVote;
+use App\Models\SurveyCandidate;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -16,7 +17,7 @@ return new class extends Migration
         Schema::create('candidate_votes', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
-            $table->foreignIdFor(CandidateVote::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(SurveyCandidate::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
